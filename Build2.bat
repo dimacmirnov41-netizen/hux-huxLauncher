cf0afe8a3450db4764c5d52e7c59ecfccbf6356c
ky4ka_boroxla <dimacmirnov41@gmail.com>
2026-08-06 13:32:06 +0300

    Add files via upload

---
+++ Build2.bat
@@ -0,0 +1,6 @@
+@echo off
+if exist dist rmdir /s /q dist
+python -m PyInstaller --clean --noconsole --onefile --uac-admin --name "hux-huxLauncher" --icon=icon.ico --add-data "hux.gif;." hux-huxLauncher.py
+if exist build rmdir /s /q build
+if exist hux-huxLauncher.spec del /f /q hux-huxLauncher.spec
+pause
