90eafa6ba55f2e00845c04d95b7d5f0ed4f4d3e4
ky4ka_boroxla <dimacmirnov41@gmail.com>
2026-08-07 20:59:37 +0300

    Update Build2.bat

--- Build2.bat
+++ Build2.bat
@@ -1,9 +1,7 @@
 :: вот кому надо кто хочет сделать
 @echo off
 if exist dist rmdir /s /q dist
-if exist hux-huxLauncher.build rmdir /s /q hux-huxLauncher.build
-if exist hux-huxLauncher.onefile-build rmdir /s /q hux-huxLauncher.onefile-build
-
-python -m nuitka --standalone --onefile --plugin-enable=tk-inter --jobs=12 --windows-uac-admin --windows-console-mode=disable --windows-icon-from-ico=icon.ico --include-data-files=hux.gif=hux.gif --include-data-files=icon.ico=icon.ico --output-filename="hux-huxLauncher" --output-dir=dist hux-huxLauncher.py
-
+python -m PyInstaller --clean --noconsole --onefile --uac-admin --name "hux-huxLauncher" --icon=icon.ico --add-data "hux.gif;." hux-huxLauncher.py
+if exist build rmdir /s /q build
+if exist hux-huxLauncher.spec del /f /q hux-huxLauncher.spec
 pause
